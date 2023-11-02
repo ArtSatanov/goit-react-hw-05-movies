63648e4139bd6b2a88d5107aa0cbb294ec3ff7b1
ArtSatanov <artsatanov@gmail.com>
2023-11-02 22:21:10 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,23 @@
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+
+const NotFound = lazy(() => import('./pages/NotFound/NotFound'));
+const Home = lazy(() => import('./pages/Home/Home'));
+const Movies = lazy(() => import('./pages/Movies/Movies'));
 
 export const App = () => {
   return (
     <div>
-      <nav>
-        <NavLink to="/" end>
-          Home
-        </NavLink>
-        <NavLink to="/movies">Movies</NavLink>
-      </nav>
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Home />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<MovieDetails />} />
+            <Route path="reviews" element={<MovieDetails />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </div>
   );
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,12 @@
+export const SharedLayout = () => {
+  return (
+    <div>
+      <nav>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+    </div>
+  );
+};
