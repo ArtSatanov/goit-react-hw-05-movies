792f7f5c5b2f4bdb57c1a660d91fa2526e0829e0
ArtSatanov <artsatanov@gmail.com>
2023-11-03 13:24:45 +0200

    1

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,7 +1,17 @@
-export const MoviesList = () => {
+import { Link } from 'react-router-dom';
+
+export const MoviesList = ({ response }) => {
   return (
     <div>
-      <ul></ul>
+      <ul>
+        {response.map(resp => (
+          <li key={resp.id}>
+            <Link to={`movies/: ${resp.id}`}>
+              {resp.name} ?? {resp.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
